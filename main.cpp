558fb3d7e9640c4c0537d977c7278b7a0096932a
krzystof6563 <krzysztofmichalski42@gmail.com>
2019-11-10 11:06:52 +0100

    Hello world #2

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    cout << "Content-type: text/html\n\n" << endl;
+    cout << "<html>Hello world!</html>" << endl;
     return 0;
 }
